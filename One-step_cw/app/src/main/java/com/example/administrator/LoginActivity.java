ab34c5bbdbaa73f004c9bf0fc2a756a767194261
cwabc <cwfun@qq.com>
2018-11-30 13:21:18 +0800

    合并了前端界面

--- One-step_cw/app/src/main/java/com/example/administrator/LoginActivity.java
+++ One-step_cw/app/src/main/java/com/example/administrator/LoginActivity.java
@@ -29,6 +29,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,9 +37,11 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static android.Manifest.permission.READ_CONTACTS;
 import com.example.administrator.tools.MD5Util;
+import com.example.administrator.tools.analyzeJson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -51,11 +54,24 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+
 public class LoginActivity extends AppCompatActivity{
+    private String email = new String("");
+    private String password = new String("");
+    private int state = 3;
+    private Button login;
+    private  TextView t1;
+    private  TextView t2;
+    private CheckBox ck;
+
+
+    @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView title = (TextView)findViewById(R.id.title_name);
+        title.setText("登录账号");
         Button back = (Button)findViewById(R.id.title_button1);
         back.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -65,59 +81,119 @@ public class LoginActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
-        Button login = (Button)findViewById(R.id.sign_in);
+        TextView forGetPwd = (TextView) findViewById(R.id.loginForgetPwd);
+        forGetPwd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent=new Intent(LoginActivity.this,ForgetPwd.class);
+                startActivity(intent);
+            }
+        });
+
+
+        login = (Button)findViewById(R.id.sign_in);
+        t1 = (TextView) findViewById(R.id.email);
+        t2 =(TextView) findViewById(R.id.passwordL);
+        ck = (CheckBox) findViewById(R.id.rememberPwd);
         login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view)
             {
-                Intent intent=new Intent(LoginActivity.this,homePage.class);
-                startActivity(intent);
+
+                email = t1.getText().toString();
+                password = t2.getText().toString();
+
+                if (t1.getText().length()==0 ||t2.getText().length()==0)
+                {
+                    Toast.makeText(LoginActivity.this,"用户名或密码不能为空",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                 t1.setText(email);
+                 if(ck.isChecked())
+                 {
+                     t2.setText(password);
+                 }
+                WorkThread sendMessage = new WorkThread();
+                sendMessage.start();
+                try {
+                    sendMessage.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                sendMessage.interrupt();
+                state = 0;
+                switch (state){
+                    case 0 :{
+                        Intent intent = new Intent(LoginActivity.this,Main2Activity.class);
+                        startActivity(intent);
+                        break;
+                    }
+                    case 1: {
+                        Toast.makeText(LoginActivity.this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case 2: {
+                        Toast.makeText(LoginActivity.this, "服务器连接失败", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    default:{
+                        Toast.makeText(LoginActivity.this, "未知错误", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+                return;
             }
         });
-        /*login.setOnClickListener();
-        login.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void  onClick(View view)
-            {
-                sendMessage();
-            }
-        });*/
+
     }
-    private void sendMessage()
+
+
+    private  class WorkThread extends Thread
     {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try{
-                        OkHttpClient client = new OkHttpClient();
-                        String p = "{\"user_name\":\"test\",\"user_pwd\":\"test\"}";
-                        MediaType JSON = MediaType.parse("text/html; charset=utf-8");
-                        RequestBody body = RequestBody.create(JSON, p);
-                        Request request = new Request.Builder().url("http://115.159.198.216/YibuTest/Login").post(body).build();
-                        Response response = client.newCall(request).execute();
-                        String responseData = response.body().string();
-                        Log.d("LoginActivity",responseData);
-                        //parseJSONWithJSONObject(responseData);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient.Builder()
+                            .connectTimeout(10, TimeUnit.SECONDS)
+                            .writeTimeout(10, TimeUnit.SECONDS)
+                            .readTimeout(20, TimeUnit.SECONDS)
+                            .build();
+
+                    JSONObject emailAndPwd = new JSONObject();
+                    emailAndPwd.put("e-mail", email);
+                    emailAndPwd.put("userPwd", password);
+
+                    MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+                    RequestBody body = RequestBody.create(JSON, emailAndPwd.toString());
+
+                    Request request = new Request.Builder().url("http://115.159.198.216/YibuTest/Login").post(body).build();
+                    Response response = client.newCall(request).execute();
+
+                    String responseData = new String("");
+                    if (response.isSuccessful()) {
+                        responseData = response.body().string();
+
+                        String answer=analyzeJson.getJsonString(responseData);
+                        Log.d("Login",answer);
+                        if(answer.equals("Yes"))
+                            state = 0;
+                        else
+                            state = 1;
+                    } else {
+                        state =2 ;
+                        throw new IOException("无法连接到服务器，请检查网络连接");
                     }
 
+                    //parseJSONWithJSONObject(responseData);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            }) .start();
-
-
-    }
-    private void parseJSONWithJSONObject(String jsonData)
-    {
-            try {
-                JSONObject jsonObject = new JSONObject(jsonData);
-                String name=jsonObject.getString("answer");
-                Toast.makeText(LoginActivity.this,name,Toast.LENGTH_SHORT).show();
-                } catch (JSONException e) {
-                e.printStackTrace();
             }
     }
+
 
 }
 
--- One-step_cw/app/src/main/java/com/example/administrator/tools/HttpCallbackListener.java
+++ One-step_cw/app/src/main/java/com/example/administrator/tools/HttpCallbackListener.java
@@ -3,6 +3,5 @@ package com.example.administrator.tools;
 public interface HttpCallbackListener {
 
     void onFinish(String response);
-
     void onError(Exception e);
 }
